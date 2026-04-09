b8105dd34a264054b4147f602698d7a807a95158
Misha1796 <150789896+Misha1796@users.noreply.github.com>
2026-04-09 15:58:26 +0300

    Update kinogo.js

--- kinogo.js
+++ kinogo.js
@@ -1,29 +1,32 @@
 (function () {
     'use strict';
 
-    function searchKinogo(title, year, callback) {
-        let query = encodeURIComponent(title + ' ' + (year || ''));
+    // Функция поиска ссылки на Kinogo
+    function searchKinogo(title, callback) {
+        let query = encodeURIComponent(title);
         let url = 'https://kinogo.online/index.php?do=search&subaction=search&story=' + query;
 
-        fetch(url, {
-            method: 'GET'
-        })
-        .then(r => r.text())
-        .then(html => {
-            let match = html.match(/href="(https:\/\/kinogo\.online\/[^"]+\.html)"/);
-            if (match) callback(match[1]);
-            else callback(null);
-        })
-        .catch(() => callback(null));
+        fetch(url)
+            .then(response => response.text())
+            .then(html => {
+                let match = html.match(/href="(https:\/\/kinogo\.online\/serialy\/[^"]+\.html)"/i);
+                if (!match) match = html.match(/href="(https:\/\/kinogo\.online\/film\/[^"]+\.html)"/i);
+                callback(match ? match[1] : null);
+            })
+            .catch(() => callback(null));
     }
 
+    // Функция открытия Kinogo
     function openKinogo(card) {
-        let title = card.title || card.original_title;
-        let year = card.release_date ? card.release_date.split('-')[0] : '';
+        let title = card.title || card.original_title || '';
+        if (!title) {
+            Lampa.Noty.show('Название не найдено');
+            return;
+        }
 
-        Lampa.Noty.show('Поиск Kinogo...');
+        Lampa.Noty.show('Поиск на Kinogo...');
 
-        searchKinogo(title, year, function (url) {
+        searchKinogo(title, function(url) {
             if (!url) {
                 Lampa.Noty.show('Не найдено на Kinogo');
                 return;
@@ -36,14 +39,16 @@
         });
     }
 
-    Lampa.Listener.follow('full', function (e) {
-        if (e.type == 'complite') {
+    // Добавляем кнопку в карточку фильма
+    Lampa.Listener.follow('full', function(e) {
+        if (e.type === 'complite') {
             let card = e.data.movie;
 
+            // Кнопка ▶ Kinogo
             e.object.append({
                 title: '▶ Kinogo',
                 icon: 'play',
-                onSelect: function () {
+                onSelect: function() {
                     openKinogo(card);
                 }
             });
