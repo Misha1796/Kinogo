cad9e311e17d6c9dc2295212d2daaba753f789fa
Misha1796 <150789896+Misha1796@users.noreply.github.com>
2026-04-09 16:06:47 +0300

    Update kinogo.js

--- kinogo.js
+++ kinogo.js
@@ -1,57 +1,50 @@
-(function () {
+(function() {
     'use strict';
 
+    // Добавляем кнопку ▶ Kinogo в карточку фильма
+    function addKinogoButton(card, object) {
+        object.append({
+            title: '▶ Kinogo',
+            icon: 'play',
+            onSelect: function() {
+                if(!card.title){
+                    Lampa.Noty.show('Название фильма не найдено');
+                    return;
+                }
+                Lampa.Noty.show('Поиск на Kinogo...');
+                searchKinogo(card.title, function(url){
+                    if(url){
+                        Lampa.Player.play({title: card.title, url: url});
+                    } else {
+                        Lampa.Noty.show('Фильм не найден на Kinogo');
+                    }
+                });
+            }
+        });
+    }
+
     // Функция поиска ссылки на Kinogo
-    function searchKinogo(title, callback) {
-        let query = encodeURIComponent(title);
-        let url = 'https://kinogo.online/index.php?do=search&subaction=search&story=' + query;
+    function searchKinogo(title, callback){
+        var query = encodeURIComponent(title);
+        var url = 'https://kinogo.online/index.php?do=search&subaction=search&story=' + query;
 
         fetch(url)
             .then(response => response.text())
             .then(html => {
-                // Ищем ссылку на сериал или фильм
-                let match = html.match(/href="(https:\/\/kinogo\.online\/serialy\/[^"]+\.html)"/i);
-                if (!match) match = html.match(/href="(https:\/\/kinogo\.online\/film\/[^"]+\.html)"/i);
+                // Сначала ищем сериал, затем фильм
+                var match = html.match(/href="(https:\/\/kinogo\.online\/serialy\/[^"]+\.html)"/i);
+                if(!match) match = html.match(/href="(https:\/\/kinogo\.online\/film\/[^"]+\.html)"/i);
                 callback(match ? match[1] : null);
             })
             .catch(() => callback(null));
     }
 
-    // Открытие Kinogo через встроенный плеер Lampa
-    function openKinogo(card) {
-        let title = card.title || card.original_title || '';
-        if (!title) {
-            Lampa.Noty.show('Название фильма не найдено');
-            return;
-        }
-
-        Lampa.Noty.show('Поиск на Kinogo...');
-        searchKinogo(title, function(url) {
-            if (!url) {
-                Lampa.Noty.show('Не найдено на Kinogo');
-                return;
-            }
-
-            // Запуск плеера Lampa
-            Lampa.Player.play({
-                title: title,
-                url: url
-            });
-        });
-    }
-
-    // Добавляем кнопку в карточку фильма
-    Lampa.Listener.follow('full', function(e) {
-        if (e.type === 'complite') {
-            let card = e.data.movie;
-
-            e.object.append({
-                title: '▶ Kinogo',
-                icon: 'play',
-                onSelect: function() {
-                    openKinogo(card);
-                }
-            });
+    // Отслеживаем открытие карточки фильма
+    Lampa.Listener.follow('full', function(e){
+        if(e.type === 'complite'){
+            var card = e.data.movie;
+            addKinogoButton(card, e.object);
         }
     });
+
 })();
